7db61f2b0067de4f0a5219adb9012071b9010ac1
51pwn <18223385+hktalent@users.noreply.github.com>
2022-04-11 22:08:59 +0800

    add

---
+++ Formula/DHTSpider2ES.rb
@@ -0,0 +1,14 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class DHTSpider2ES < Formula
+    desc "Cross-platform An optimized DHT crawler capable of sending data to Elasticsearch"
+    homepage "http://github.com/hktalent/dht"
+    url "https://github.com/hktalent/dht/releases/download/V1.0.0/DHTSpider2Elasticsearch-mac.tar.gz"
+    sha256 "5936e3850099ce3077ccdda55501e6b339eb21e090860fcc48634e7f7e360a1c"
+    version "V1.0.0"
+  
+    def install
+      bin.install "DHTSpider2ES"
+    end
+  end
